b53ac7db58167e0edf4fed1148d2e8bce853b4dc
Anderson Menezes <andersonmenezes.dev@gmail.com>
2025-10-19 12:32:36 -0300

    1.6 - configuração do projeto users-api e auth-api

---
+++ apps/auth-api/kysely.config.ts
@@ -0,0 +1,35 @@
+import {
+  CamelCasePlugin,
+  DeduplicateJoinsPlugin,
+  PostgresDialect,
+} from 'kysely';
+import { defineConfig } from 'kysely-ctl';
+import { Pool } from 'pg';
+
+export default defineConfig({
+  dialect: () => {
+    const databaseUrl = new URL(process.env.DATABASE_URL ?? '');
+    return new PostgresDialect({
+      pool: new Pool({
+        connectionString: databaseUrl.toString(),
+      }),
+    });
+  },
+  migrations: {
+    migrationFolder: './database/migrations',
+  },
+  plugins: [new CamelCasePlugin(), new DeduplicateJoinsPlugin()],
+  $env: {
+    test: {
+      dialect: () => {
+        const testDatabaseUrl = new URL(process.env.DATABASE_URL ?? '');
+        testDatabaseUrl.pathname = '/test';
+        return new PostgresDialect({
+          pool: new Pool({
+            connectionString: testDatabaseUrl.toString(),
+          }),
+        });
+      },
+    },
+  },
+});
