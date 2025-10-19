e0fa4164c0abde123a5a390ee5c80bc08f2bf068
Anderson Menezes <andersonmenezes.dev@gmail.com>
2025-10-19 12:48:34 -0300

    2.0 - remoção de scripts de tabela SQL e ajustes em configurações do projeto

--- apps/users-api/database/migrations/1760887794975_create-user-table.ts
+++ apps/users-api/database/migrations/1760887794975_create-user-table.ts
@@ -1,15 +1,29 @@
-import type { Kysely } from 'kysely'
+import { sql, type Kysely } from 'kysely';
 
 // `any` is required here since migrations should be frozen in time. alternatively, keep a "snapshot" db interface.
 export async function up(db: Kysely<any>): Promise<void> {
-	// up migration code goes here...
-	// note: up migrations are mandatory. you must implement this function.
-	// For more info, see: https://kysely.dev/docs/migrations
+	await db.schema
+		.createTable('user')
+		.addColumn('sub_id', 'uuid', (col) =>
+			col.primaryKey().defaultTo(sql`gen_random_uuid()`),
+		)
+		.addColumn('email', 'text', (col) => col.notNull().unique())
+		.addColumn('first_name', 'text', (col) => col.notNull())
+		.addColumn('last_name', 'text', (col) => col.notNull())
+		.addColumn('role', 'text')
+		.addColumn('biography', 'text')
+		.addColumn('interests', 'jsonb', (col) => col.defaultTo(sql`'[]'::jsonb`))
+		.addColumn('profilePictureUrl', 'text')
+		.addColumn('created_at', 'timestamptz', (col) =>
+			col.notNull().defaultTo(sql`now()`),
+		)
+		.addColumn('updated_at', 'timestamptz', (col) =>
+			col.notNull().defaultTo(sql`now()`),
+		)
+		.execute();
 }
 
 // `any` is required here since migrations should be frozen in time. alternatively, keep a "snapshot" db interface.
 export async function down(db: Kysely<any>): Promise<void> {
-	// down migration code goes here...
-	// note: down migrations are optional. you can safely delete this function.
-	// For more info, see: https://kysely.dev/docs/migrations
+	await db.schema.dropTable('user').execute();
 }
