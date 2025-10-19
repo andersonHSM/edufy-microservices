ccbc845d87801a341dd98e9f00d9bc4ae3bfb046
Anderson Menezes <andersonmenezes.dev@gmail.com>
2025-10-19 14:33:57 -0300

    2.1 - ajustes em tabelas, scripts de migração e configurações gerais do projeto

--- apps/auth-api/database/migrations/1760887820630_create-user-table.ts
+++ apps/auth-api/database/migrations/1760887820630_create-user-table.ts
@@ -3,7 +3,9 @@ import { sql, type Kysely } from 'kysely';
 // `any` is required here since migrations should be frozen in time. alternatively, keep a "snapshot" db interface.
 export async function up(db: Kysely<any>): Promise<void> {
 	await db.schema
-		.createTable('user')
+		.withSchema('auth')
+		.createTable('users')
+		.ifNotExists()
 		.addColumn('sub', 'uuid', (col) =>
 			col.primaryKey().defaultTo(sql`gen_random_uuid()`),
 		)
@@ -21,5 +23,5 @@ export async function up(db: Kysely<any>): Promise<void> {
 
 // `any` is required here since migrations should be frozen in time. alternatively, keep a "snapshot" db interface.
 export async function down(db: Kysely<any>): Promise<void> {
-	await db.schema.dropTable('user').execute();
+	await db.schema.withSchema('users').dropTable('auth.users').ifExists().execute();
 }
--- apps/users-api/database/migrations/1760887794975_create-user-table.ts
+++ apps/users-api/database/migrations/1760887794975_create-user-table.ts
@@ -3,11 +3,10 @@ import { sql, type Kysely } from 'kysely';
 // `any` is required here since migrations should be frozen in time. alternatively, keep a "snapshot" db interface.
 export async function up(db: Kysely<any>): Promise<void> {
 	await db.schema
-		.createTable('user')
-		.addColumn('sub_id', 'uuid', (col) =>
-			col.primaryKey().defaultTo(sql`gen_random_uuid()`),
-		)
-		.addColumn('email', 'text', (col) => col.notNull().unique())
+		.withSchema('users')
+		.createTable('users')
+		.addColumn('sub_id', 'uuid')
+		.addColumn('email', 'text', (col) => col.notNull().unique().primaryKey())
 		.addColumn('first_name', 'text', (col) => col.notNull())
 		.addColumn('last_name', 'text', (col) => col.notNull())
 		.addColumn('role', 'text')
@@ -25,5 +24,5 @@ export async function up(db: Kysely<any>): Promise<void> {
 
 // `any` is required here since migrations should be frozen in time. alternatively, keep a "snapshot" db interface.
 export async function down(db: Kysely<any>): Promise<void> {
-	await db.schema.dropTable('user').execute();
+	await db.schema.withSchema('users').dropTable('users').execute();
 }
