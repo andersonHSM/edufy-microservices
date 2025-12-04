a36b94a7b2a4979a53c3162be1a91ede7af38af2
Anderson Menezes <andersonmenezes.dev@gmail.com>
2025-12-04 15:21:18 -0300

    chore: reorganização de migrations no auth-api, inclusão das dependências `argon2` e `zod`, ajustes no arquivo `pnpm-lock.yaml` e atualizações referentes na configuração do Kysely

--- apps/auth-api/kysely.config.ts
+++ apps/auth-api/kysely.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
 		});
 	},
 	migrations: {
-		migrationFolder: './database/migrations',
+		migrationFolder: './src/libs/database/migrations',
 	},
 	plugins: [new CamelCasePlugin(), new DeduplicateJoinsPlugin()],
 	$env: {
