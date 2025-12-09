960b1be8f31275f88fc240fe4f338464165f4350
Anderson Menezes <andersonmenezes.dev@gmail.com>
2025-12-09 15:40:31 -0300

    4.4: processamento do evento `user_signed_up` com criação de usuário e gerenciamento de mensagens via RabbitMQ

--- apps/users-api/src/app/users/application/users.service.ts
+++ apps/users-api/src/app/users/application/users.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { RpcException } from '@nestjs/microservices';
+import { UserEntity } from 'src/app/users/domain/user.entity';
+import {
+  type IUserRepository,
+  UserRepository,
+} from 'src/app/users/domain/user.repository';
+import { UserSignedUpEvent } from 'src/app/users/events/user-signed-up.event';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @Inject(UserRepository) private readonly userRepository: IUserRepository,
+  ) {}
+  public async createUser(payload: UserSignedUpEvent) {
+    const userExists = await this.userRepository.findOneByEmail(payload.email);
+    if (userExists) {
+      throw new RpcException({
+        message: 'Email already in use',
+        code: 409,
+      });
+    }
+
+    const newUser = UserEntity.create(payload);
+
+    await this.userRepository.save(newUser);
+    return newUser;
+  }
+}
--- apps/users-api/src/app/users/presentation/users.controller.ts
+++ apps/users-api/src/app/users/presentation/users.controller.ts
@@ -1,7 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Logger } from '@nestjs/common';
+import { Ctx, EventPattern, Payload, RmqContext } from '@nestjs/microservices';
 import { UsersService } from 'src/app/users/application/users.service';
+import { UserSignedUpEvent } from 'src/app/users/events/user-signed-up.event';
 
 @Controller('users')
 export class UsersController {
+  private readonly logger = new Logger(UsersController.name);
   constructor(private readonly usersService: UsersService) {}
+
+  @EventPattern('user_signed_up')
+  public async handleUserSignedUp(
+    @Payload() payload: UserSignedUpEvent,
+    @Ctx() context: RmqContext,
+  ) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const channel = context.getChannelRef();
+    const originalMsg = context.getMessage();
+
+    try {
+      await this.usersService.createUser(payload);
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access
+      channel.ack(originalMsg);
+      this.logger.log(`Acknoledged message for user: ${payload.email}`);
+    } catch (error) {
+      this.logger.error(
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        `Failed to process message for user: ${payload.email}. Sending to DLQ. Error: ${error.message}`,
+      );
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access
+      channel.nack(originalMsg, false, false);
+    }
+  }
 }
