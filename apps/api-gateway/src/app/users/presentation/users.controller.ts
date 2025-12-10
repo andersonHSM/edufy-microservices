afb1409ff4c5e0c8fba8c551dcb3a77e7429596f
Anderson Menezes <andersonmenezes.dev@gmail.com>
2025-12-10 15:12:03 -0300

    refactor: ajusta tipagem e melhorias nos logs, adiciona decorator `ApiBearerAuth` e validações com Swagger

--- apps/api-gateway/src/app/users/presentation/users.controller.ts
+++ apps/api-gateway/src/app/users/presentation/users.controller.ts
@@ -7,6 +7,7 @@ import {
   Post,
   UseFilters,
 } from "@nestjs/common";
+import { ApiBearerAuth } from "@nestjs/swagger";
 import { UsersService } from "src/app/users/application/users.service";
 import { AssignRoleDto } from "src/app/users/presentation/dto/assign-role.dto";
 import { SignupUserDto } from "src/app/users/presentation/dto/signup-user.dto";
@@ -16,29 +17,36 @@ import { RpcToHttpExceptionFilter } from "src/libs/exception-filters/rpc-to-http
 import { CurrentUser } from "./current-user.decorator";
 
 @Controller("users")
+@UseFilters(new RpcToHttpExceptionFilter())
+@ApiBearerAuth()
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Public()
-  @UseFilters(new RpcToHttpExceptionFilter())
   @Post("")
   public createUser(@Body() body: SignupUserDto) {
     return this.usersService.createUser(body);
   }
 
   @Get("me")
-  public getMe(@CurrentUser() user: any) {
-    return this.usersService.getUserById(user.sub);
+  public getMe(@CurrentUser() userSub: string) {
+    return this.usersService.getUserById(userSub);
   }
 
   @Patch("me")
-  public updateUser(@CurrentUser() user: any, @Body() body: UpdateUserDto) {
-    return this.usersService.updateUser(user.sub, body);
+  public updateUser(
+    @CurrentUser() userSub: string,
+    @Body() body: UpdateUserDto,
+  ) {
+    return this.usersService.updateUser(userSub, body);
   }
 
   @Post("me/role")
-  public assignRole(@CurrentUser() user: any, @Body() body: AssignRoleDto) {
-    return this.usersService.assignRole(user.sub, body.role);
+  public assignRole(
+    @CurrentUser() userSub: string,
+    @Body() body: AssignRoleDto,
+  ) {
+    return this.usersService.assignRole(userSub, body.role);
   }
 
   @Get(":subId")
